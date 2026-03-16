b1aa133bb443c8e2b76a9d5cbd667b9cfb8306ab
unknown <avapowelson@gmail.com>
2026-03-16 09:47:46 -0300

    minor edits

--- data/preprocess.py
+++ data/preprocess.py
@@ -16,35 +16,40 @@ df = pd.read_csv("wsn-ds.csv")
 print(f"Number of Rows x Columns: {df.shape[0]} x {df.shape[1]}\n")
 print(f"Class distribution:\n {df.iloc[:, -1].value_counts()}\n")
 
-# No null cells encountered
+"""
+No null cells encountered
+"""
 # print(df.isnull().sum())
 
-# Remove outliers
+"""
+Handle Outliers
+- IQR 25-75 removes everything except TDMA and Normal, so we'll just keep everything
+"""
 # numerical_cols = df.select_dtypes(include=['float64', 'int64']).columns
 # q1 = df[numerical_cols].quantile(0.25)
 # q3 = df[numerical_cols].quantile(0.75)
 # iqr = q3 - q1
 # df_no_outliers = df[~((df[numerical_cols] < (q1 - 1.5*iqr)) | (df[numerical_cols] > (q3 + 1.5*iqr))).any(axis=1)]
-
 # print(f"Outliers Removed Rows: {df.shape[0]}")
 
-# Class distribution
+"""
+Check distribution
+"""
 class_counts = df['label'].value_counts()
 print(f"Class distro:\n {class_counts}")
 
-# Balance using SMOTE
+"""
+Balance using SMOTE
+"""
 data = df.iloc[:, :-1]
 lbl = df.iloc[:, -1]
-
 smote = SMOTE(random_state=42)
 data_resampled, lbl_resampled = smote.fit_resample(data, lbl)
-
+df_resampled = pd.concat([data_resampled, lbl_resampled], axis=1)
+print(f"Resampled data shape: {df_resampled.shape}")
 # sns.countplot(x=lbl_resampled)
 # plt.title("Class Distro after SMOTE")
 # plt.show()
-
-df_resampled = pd.concat([data_resampled, lbl_resampled], axis=1)
-print(f"Resampled data shape: {df_resampled.shape}")
 
 # Feature Engineering TBD
 ...
