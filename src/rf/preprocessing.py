2f8bc9c318a80eae0cd62d77d08bc8a29e1cbc38
AminaK2724 <105371223+AminaK2724@users.noreply.github.com>
2026-03-19 04:28:40 -0300

    rf preprocessing function
    Added a function to load and preprocess data from a CSV file, including cleaning column names, removing duplicates, and creating binary labels.

--- src/rf/preprocessing.py
+++ src/rf/preprocessing.py
@@ -1,1 +1,45 @@
-hello
+"""
+Amina Fatma Khan
+B00868087
+March 18, 2026
+
+See README.md for references.
+"""
+
+import pandas as pd
+import numpy as np
+
+def load_data():
+    df = pd.read_csv("WSN-DS.csv")
+
+    print(df.shape)
+    print(df.head())
+    print(df.columns.tolist())
+
+    df.columns = (
+        df.columns
+        .str.strip()
+        .str.replace(" ", "_")
+        .str.replace("-", "_")
+        .str.replace("/", "_")
+    )
+    print(df.columns.tolist())
+  
+    print(df.info())
+    print(df.isnull().sum())
+    print(df.duplicated().sum())
+    print(df['label'].value_counts())
+
+    # Remove duplicate rows, reset index and drop missing values
+    df = df.drop_duplicates().reset_index(drop=True)
+    df = df.dropna()
+
+    # Binary labels - "Normal" or "Attack"
+    df['binary_label'] = df['label'].apply(lambda x: 'Normal' if x == 'Normal' else 'Attack')
+    print(df['binary_label'].value_counts())
+
+    drop_cols = ['id', 'label', 'binary_label']
+    X = df.drop(columns=drop_cols)
+    y = df['binary_label']
+
+    return df, X, y
